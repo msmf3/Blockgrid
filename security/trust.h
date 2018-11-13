a8fa5eaffd58848cac96640740d4fa2aa559e79b
learnitdeep <learnitdeep@gmail.com>
2018-11-13 13:57:32 +0000

    add security API

---
+++ security/trust.h
@@ -0,0 +1,13 @@
+#ifndef TEE_API
+#define TEE_API
+
+#include <artik/security.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+#endif
